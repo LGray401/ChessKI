a26526b7eac0d1aeed724b40ec40715638f77a29
tulin <tulinht@yahoo.de>
2023-04-27 16:52:10 +0200

    im schach methode in der boardklasse

--- src/Board.java
+++ src/Board.java
@@ -17,12 +17,12 @@ public class Board {
         Figure[] board = new Figure[63];
 
         // add white pawns
-        for(int i = 8; i <= 15; i++) {
+        for (int i = 8; i <= 15; i++) {
             board[i] = new Pawn(false, i, new ArrayList<>(Arrays.asList(7, 8, 9)));
         }
 
         // add black pawns
-        for(int i = 48; i <= 55; i++) {
+        for (int i = 48; i <= 55; i++) {
             board[i] = new Pawn(true, i, new ArrayList<>(Arrays.asList(-7, -8, -9)));
         }
 
@@ -51,11 +51,48 @@ public class Board {
 
         // add empty fields
 
-        for(int i = 16; i <= 55; i++) {
+        for (int i = 16; i <= 55; i++) {
             board[i] = new EmptyField();
         }
 
         return board;
+
     }
+
+        /*
+        public boolean notOver() {
+
+            for (int i = 0; i< 64;i++)
+            {
+                if( board[i]== King.position )
+                {
+                    if(i >32) {
+                        return false;
+                    }
+
+                }
+
+            }
+
+
+
+            return true;
+            // check if game is finished
+        }
+
+        public boolean imSchach(Spieler spieler1, Spieler spieler2) {
+            boolean schach;
+
+            for (Figure figure: spieler.listeVonFigures){
+                for (int possibleMove : figure.possibleMoves) { //bzw legalMoves hier
+                    int posKingOfOpponent = ...
+                    if (posKingOfOpponent == possibleMove){
+                        schach = true;
+                    }
+                }
+            }
+        }
+         */
+
 
 }
