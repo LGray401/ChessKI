d4292bb0a25fa1117874d973b00cfa9bb0641234
rudi_hermann <rudolph.hermann@campus.tu-berlin.de>
2023-05-04 20:56:19 +0200

    .

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -13,23 +13,13 @@ public class Main {
         Player player1 = new Player(false);
         Player player2 = new Player(true);
 
-        board.getBoard()[25] = new Bishop(false, 25);
-        board.getBoard()[24] = new Rook(false, 24);
-        board.getBoard()[28] = new Queen(false, 28);
-
-
-
-
         player1.playerGetFigureList(board);
         player1.generateAllMoves(board);
         player2.playerGetFigureList(board);
         player2.generateAllMoves(board);
 
-        //for (Figure figure: player1.getFigureList()) {System.out.println("Possible Moves of " + figure.getClass().getName() + ": " + figure.getPossibleMoveList());}
-        //for (Figure figure: player2.getFigureList()) {System.out.println("Possible Moves of " + figure.getClass().getName() + ": " + figure.getPossibleMoveList());}
-
         System.out.println("All legal moves for player1: " + player1.getAllMovesInFenNotation());
-        //for (Figure figure: player2.getFigureList()) {System.out.println("Possible Moves of " + figure.getClass().getName() + ": " + figure.getPossibleMoveList());}
+        System.out.println("All legal moves for player1: " + player1.getAllMovesInFenNotation());
 
     }
 }
