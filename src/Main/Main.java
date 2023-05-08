0192a4fec4390bbf815366a0a4a028e53c06c218
Ludwig <ludwig.roeber@sqlxpert.de>
2023-05-08 21:43:55 +0200

    changed main to execute full game

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,25 +1,38 @@
 package Main;
 
-import Figures.Bishop;
-import Figures.Figure;
-import Figures.Queen;
-import Figures.Rook;
+import Figures.*;
+
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
         Board board = new Board();
-        board.setBoard(Board.intialize());
+        board.setBoard(board.intialize());
         Player player1 = new Player(false);
         Player player2 = new Player(true);
 
-        player1.playerGetFigureList(board);
-        player1.generateAllMoves(board);
-        player2.playerGetFigureList(board);
-        player2.generateAllMoves(board);
 
-        System.out.println("All legal moves for player1: " + player1.getAllMovesInFenNotation());
-        System.out.println("All legal moves for player2: " + player2.getAllMovesInFenNotation());
+        while (true ) {
+            board.isGameOver(player2.isBlack());
+            Figure nextMove = player1.makeMove(board);
+            nextMove.setNextPosition(nextMove.getPossibleMoveList().get((int) (Math.random() * nextMove.getPossibleMoveList().size())));
+            System.out.println("Player1 moved " + nextMove.getClass().getSimpleName() +" from: " + nextMove.getPosition());
+            board.changeBoard(nextMove);
+            System.out.println("Player1 moved to: " + nextMove.getNextPosition());
+            board.isGameOver(player1.isBlack());
+            nextMove = player2.makeMove(board);
+            nextMove.setNextPosition(nextMove.getPossibleMoveList().get((int) (Math.random() * nextMove.getPossibleMoveList().size())));
+            System.out.println("Player2 moved " + nextMove.getClass().getSimpleName() +" from: " + nextMove.getPosition());
+            board.changeBoard(nextMove);
+            System.out.println("Player2 moved to: " + nextMove.getNextPosition());
+            board.to2DArrayAndDisplay(board.getBoard());
+            //System.out.println(board.createFENFromBoard(board.getBoard()));
+        }
+
+
+        //Figure[] testBoard = Board.createBoardFromFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
+
 
     }
 }
